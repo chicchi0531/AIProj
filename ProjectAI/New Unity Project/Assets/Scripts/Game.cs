ebb6b416fdde662165a98691b675c41ab09c12f1
chicchi0531 <coi_mp@yahoo.co.jp>
2018-03-22 23:54:23 +0900

    2018/3/22

---
+++ ProjectAI/New Unity Project/Assets/Scripts/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    //Singleton
+    private static Game mInst;
+    public static Game GetInstance()
+    {
+        if (mInst == null)
+        {
+            GameObject gameObject = Resources.Load("Prefabs/System/Game") as GameObject;
+            mInst = Instantiate(gameObject).GetComponent<Game>();
+        }
+        return mInst;
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (mInst == null)
+        {
+            this.Setup();
+            mInst = this;
+        }
+        else if (mInst != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //初期化処理
+    void Setup()
+    {
+
+    }
+
+
+}
